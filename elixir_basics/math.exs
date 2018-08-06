966d21ac2888eba8e6185a7a6e5de63c057ea903
mgmilton <matt.milton@gmail.com>
2018-08-06 16:06:40 -0700

    adds type comparison

--- elixir_basics/math.exs
+++ elixir_basics/math.exs
@@ -8,3 +8,19 @@ IO.puts(0b0110)
 IO.puts(0o644)
 #Hex
 IO.puts(0x1F)
+
+#Comparison
+1 > 2
+1 != 2
+2 == 2
+2  < =  3
+
+# Strict Comparison of integers and floats
+2 == 2.0
+
+2 === 2.0
+
+# Types can be compared as well
+# numbers < atom < reference < function < port < pid < tuple < map < list < bitstring
+
+3 < :hello
