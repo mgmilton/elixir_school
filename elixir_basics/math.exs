778225bf188b5486540d43a71bf46fb90384081d
mgmilton <matt.milton@gmail.com>
2018-08-06 19:07:25 -0700

    adds anonymous fx

--- elixir_basics/math.exs
+++ elixir_basics/math.exs
@@ -13,7 +13,7 @@ IO.puts(0x1F)
 1 > 2
 1 != 2
 2 == 2
-2  < =  3
+2  <=  3
 
 # Strict Comparison of integers and floats
 2 == 2.0
@@ -24,3 +24,16 @@ IO.puts(0x1F)
 # numbers < atom < reference < function < port < pid < tuple < map < list < bitstring
 
 3 < :hello
+
+add = fn a, b -> a + b end
+IO.puts(add.(1, 2))
+
+is_function(add)
+
+is_function(add, 2) # check if add is a function that expects exactly 2 arguments
+
+
+#Anonymous functions are closures
+
+add = fn a, b -> add.(a,a) end 
+double = fn a -> add,(a, a) end
