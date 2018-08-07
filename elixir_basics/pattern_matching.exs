3b4987f8cc3a043c635fef18a1c43e3984dcbf2f
mgmilton <matt.milton@gmail.com>
2018-08-07 13:34:15 -0700

    adds match error explanations

--- elixir_basics/pattern_matching.exs
+++ elixir_basics/pattern_matching.exs
@@ -1,3 +1,9 @@
 x = 1
 1 = x returns 1
 2 = x returns match error
+{a, b, c } = {:hello, "world", 42}
+
+#match error - types have to be equivalent sizes
+#match error will be returned when types aren't equal
+{a, b, c } = {:hello, "world"}
+{a, b, c} = [:hello, "world",  42]
