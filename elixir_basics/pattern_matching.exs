959eabfd951369e8b8685614a767704bbbb91e19
mgmilton <matt.milton@gmail.com>
2018-08-07 13:39:07 -0700

    adds more match error conditions

--- elixir_basics/pattern_matching.exs
+++ elixir_basics/pattern_matching.exs
@@ -7,3 +7,11 @@ x = 1
 #match error will be returned when types aren't equal
 {a, b, c } = {:hello, "world"}
 {a, b, c} = [:hello, "world",  42]
+
+
+#matching on specific values
+{:ok, result} = {:ok, 13}
+result will return 13
+
+#match error if the first value in tuples are not equivalent
+{:ok, result} = {:error, :oops}
