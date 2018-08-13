5bba542c1c3bd15c8f5d94295fa7523640d1e4dd
mgmilton <matt.milton@gmail.com>
2018-08-13 07:57:21 -0700

    revises test

--- elixir_basics/todos/test/controllers/todo_controller_test.exs
+++ elixir_basics/todos/test/controllers/todo_controller_test.exs
@@ -28,8 +28,8 @@ defmodule Todos.TodoControllerTest do
         "todo" => %{
             "title" => todo.title,
             "description" => todo.description,
-            "inserted_at" => Ecto.DateTime.to_iso8601(todo.updated_at),
-            Ecto.DateTime.to_iso8601(todo.updated_at)
+            "inserted_at" => Ecto.DateTime.to_iso8601(todo.inserted_at),
+            "updated_at" => Ecto.DateTime.to_iso8601(todo.updated_at)
         }
       }
     end
